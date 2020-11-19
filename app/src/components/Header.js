580c06c27b1ab9f36e737534552cd98382e8b95f
MarkNewcomb1 <wlmmn@hotmail.com>
2020-11-19 10:34:42 -0700

    header done

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 export const Header = () => {
     return (
-        <div>
+        <div className="header">
             <h1><Link to='/'>DEMO Streaming</Link></h1>
         </div>
     )
